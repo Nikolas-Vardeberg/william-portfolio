f049e2fcd328d31239e336e5329ac786e20741ad
niklusrook <nikolas.vardeberg1@gmail.com>
2024-11-27 12:51:14 +0100

    feat: container

--- common/views/home-view.tsx
+++ common/views/home-view.tsx
@@ -6,10 +6,20 @@ import SanityImage from "../components/atoms/SanityImage";
 
 export default async function HomeView({data}: { data: Home }) {
     return(
-        <div className="container flex mx-auto flex-col">
-            <div className="prose-xl prose">
-                <PortableText value={data.entry} />
-            </div>
+        <div className="container flex mx-auto flex-col gap-y-12 my-10 bg-white">
+
+                <div className="w-full">
+                    <SanityImage 
+                        image={data.mainImage}
+                        height={1300}
+                        width={1300}
+                        className="w-full h-full"
+                    />
+                </div>
+
+                <div className="prose-xl prose">
+                    <PortableText value={data.entry} />
+                </div>
 
                 <div className="grid gap-5 grid-cols-1 xl:grid-cols-2">
                     {data.project?.map((project, k) => {
@@ -24,10 +34,10 @@ export default async function HomeView({data}: { data: Home }) {
                                             image={project.mainImage}
                                             height={500}
                                             width={500}
-                                            className="w-full aspect-[16/9]"
+                                            className="w-full aspect-[16/9] border-2 border-black"
                                         />
                                     </div>
-                                    <div className="flex text-white flex-wrap justify-between mt-2 w-full text-lg md:text-3xl">
+                                    <div className="flex text-black flex-wrap justify-between mt-2 w-full text-lg md:text-3xl">
                                         {project.title}
                                     </div>
                                 </div>
