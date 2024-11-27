41b2e2a4b0dfd888bd6c518cb1670d4b168ec2b7
niklusrook <nikolas.vardeberg1@gmail.com>
2024-11-27 10:38:45 +0100

    feat: tailwind typography

--- common/views/home-view.tsx
+++ common/views/home-view.tsx
@@ -5,7 +5,10 @@ import { Home } from "../types/root.types";
 export default async function HomeView({data}: { data: Home }) {
     return(
         <div>
-            <PortableText value={data.entry} />
+            <div className="prose prose-xl">
+                <PortableText value={data.entry} />
+            </div>
+            
             <pre>
                 {JSON.stringify(data, undefined, 2)}
             </pre>
