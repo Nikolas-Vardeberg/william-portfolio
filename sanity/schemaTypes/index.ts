e5d9d9a4cc049d8638d40bafba532e4b5875aff8
Nikolas, Vardeberg - Rek <156543836+Nikolas-Vardeberg@users.noreply.github.com>
2024-11-27 08:06:25 +0100

    feat: structure

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { post } from './pages/post.schema'
+import { richText, simpleRichText } from './objects/rich-text.schema'
+import { homepage } from './pages/homepage.schema'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [homepage, post, simpleRichText, richText],
 }
